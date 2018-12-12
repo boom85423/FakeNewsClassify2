6583397c4d131c05b7d86f64b55d30c290b9f791
boom85423 <38269155+boom85423@users.noreply.github.com>
2018-12-12 22:02:07 +0800

    update

--- run.py
+++ run.py
@@ -1,30 +1,24 @@
 from preprocess import Preprocess
 import pickle
-from preprocess import Preprocess
 import numpy as np
 import pandas as pd
+import sys, io
+
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
 if __name__ == "__main__":
-    train = Preprocess("dataset/train.csv")
+    with open("dataset/train.pkl", "wb") as f:
+        train = pickle.load(f)
+    with open("dataset/test.pkl", "wb") as f:
+        test = pickle.load(f)
+        
     print(train.train_A_D2V.shape, train.train_B_D2V.shape)
     print(train.train_A_Flag.shape, train.train_B_Flag.shape)
+    print(train.train_B_Keyword.shape)
     print(train.train_A_D2V_Claim.shape, train.train_B_D2V_Claim.shape)
     print(train.train_A_Flag_Claim.shape, train.train_B_Flag_Claim.shape)
     print(train.labelOriginal.shape, train.labelRelation.shape, train.labelRelation.shape)
 
-    validate = Preprocess("dataset/validate.csv")
-    print(validate.test_A_D2V.shape, validate.test_B_D2V.shape)
-    print(validate.test_A_Flag.shape, validate.test_B_Flag.shape)
-    print(validate.labelOriginal.shape)
-
-    test = Preprocess("dataset/test.csv")
-    print(validate.test_A_D2V.shape, validate.test_B_D2V.shape)
-    print(validate.test_A_Flag.shape, validate.test_B_Flag.shape)
-    
-    # with open("dataset/train.pkl", "wb") as f:
-    #     pickle.dump(train, f, pickle.HIGHEST_PROTOCOL)
-    # with open("dataset/validate.pkl", "wb") as f:
-    #     pickle.dump(validate, f, pickle.HIGHEST_PROTOCOL)
-    # with open("dataset/test.pkl", "wb") as f:
-    #     pickle.dump(test, f, pickle.HIGHEST_PROTOCOL)
-
+    print(test.test_A_D2V.shape, validate.test_B_D2V.shape)
+    print(test.test_A_Flag.shape, validate.test_B_Flag.shape)
+    print(test.test_B_Keyword.shape)
